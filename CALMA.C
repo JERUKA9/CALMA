07c47ae04628893a38d111674336c9da61e11cd9
JERUKA9 <jeruka9@gmail.com>
2017-08-01 07:38:38 +0200

    Update CALMA.C

--- CALMA.C
+++ CALMA.C
@@ -8,8 +8,19 @@ int a,b,c;
 float x,y,z;
 
 
+
 int main()
 {
+  printf("number 1");
+  scanf("%d",&a);
+   printf("number 2");
+  scanf("%d",&b);
+   printf("number 3");
+  scanf("%d",&c);
+  
+  h=x+y+z;
+  
+  d=a+b+c;
   
   
   
