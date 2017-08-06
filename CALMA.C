85affe4007b0dad231eb2a8bed2e8ed97b548913
JERUKA9 <jeruka9@gmail.com>
2017-08-06 14:26:55 +0200

    Update CALMA.C

--- CALMA.C
+++ CALMA.C
@@ -1,8 +1,13 @@
-/*CALMA.c*/
+,/*CALMA.c*/
 #include<string.h>
 #include<stdio.h>
 #include<math.h>
 
+int a,b,c;
+
+int x,y,z;
+
+int h,v,k;
 
 
 int main()
@@ -10,11 +15,45 @@ int main()
 
 puts("dodawanie\n");
 
+printf(" wprowadz pierwsza liczbe i nacisnij enter : ");
+scanf("%d",&a);
+
+
+printf(" wprowadz druga liczbe i nacisnij enter : ");
+scanf("%d",&b);
+
+c=a+b;
+
+printf("%d+%d=%d\n",a,b,c);
+
+
 
 puts("odejmowanie\n");
 
+printf(" wprowadz pierwsza liczbe i nacisnij enter : ");
+scanf("%d",&x);
+
+
+printf(" wprowadz druga liczbe i nacisnij enter : ");
+scanf("%d",&y);
+
+z=x-y;
+
+printf("%d-%d=%d\n",x,y,z);
+
 
   puts("mnozenie\n");
+
+printf(" wprowadz pierwsza liczbe i nacisnij enter : ");
+scanf("%d",&h);
+
+
+printf(" wprowadz druga liczbe i nacisnij enter : ");
+scanf("%d",&k);
+
+k=h*v;
+
+printf("%d*%d=%d\n",h,v,k);
 
 
   puts("dzielnie\n");
