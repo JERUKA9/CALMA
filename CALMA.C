1e2836ad6a993c657b6882ffd251a1f8cae7f9d5
JERUKA9 <jeruka9@gmail.com>
2017-08-06 10:10:56 +0200

    Update CALMA.C

--- CALMA.C
+++ CALMA.C
@@ -3,131 +3,15 @@
 #include<stdio.h>
 #include<math.h>
 
-int a,b,c;
-
-float x,y,z;
-
 
 
 int main()
 {
-  int a,b,c;
 
-  
-float x,y,z;
- 
-h=x+y+z;
-  
- d=a+b+c;
-  
-  printf("number 1");
-  
-  scanf("%d",&a);
-  
-   printf("number 2");
-  
-  scanf("%d",&b);
-  
-   printf("number 3");
-  
-  scanf("%d",&c);
-  
-  printf("number 4");
-  
-  scanf("%d",&x);
-  
-   printf("number 5");
-  
-  scanf("%d",&y);
-  
-   printf("number 6");
-  
-  scanf("%d",&z);
-  
-  for()
-    
-    case of
-      
-    if then else
-      
-      main()
-      
-      
-      
-      
-      int main()
-      
-      
-      our_function *a;
-  
-      
-      
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+  return 0;
+
+
 }
 
+/*end of file */
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*end of file*/
